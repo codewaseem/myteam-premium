13d1ee0519605302a80a84386539eba4dbd84532
Waseem Ahmed <codewaseem@gmail.com>
2020-04-20 11:25:28 +0530

    refactor feature component for reuse

--- src/components/Team/Features.tsx
+++ src/components/Team/Features.tsx
@@ -11,6 +11,7 @@ const baseStyles = (theme: Theme) => css`
     display: grid;
     justify-items: center;
     text-align: center;
+    align-items: center;
     margin-bottom: 48px;
 
     &:last-child {
@@ -62,39 +63,57 @@ const tabletStyles = css`
   )}
 `;
 
-const Features = () => (
-  <div css={(theme) => [baseStyles(theme), tabletStyles]}>
+interface FeatureItemPros {
+  Icon: React.ComponentType;
+  title: string;
+  description?: string;
+}
+
+export const FeatureItem: React.FC<FeatureItemPros> = ({
+  Icon,
+  title,
+  description,
+}) => (
+  <div>
+    <Icon />
     <div>
-      <IconPerson />
-      <div>
-        <h4>Experienced Individuals</h4>
-        <p>
-          Our network is made up of highly experienced professionals who are
-          passionate about what they do.
-        </p>
-      </div>
-    </div>
-    <div>
-      <IconCog />
-      <div>
-        <h4>Easy to Implement</h4>
-        <p>
-          Our processes have been refined over years of implementation meaning
-          our teams always deliver.
-        </p>
-      </div>
-    </div>
-    <div>
-      <IconChart />
-      <div>
-        <h4>Enhanced Productivity</h4>
-        <p>
-          Our customized platform with in-built analytics helps you manage your
-          distributed teams.
-        </p>
-      </div>
+      <h4>{title}</h4>
+      {description && <p>{description}</p>}
     </div>
   </div>
+);
+
+interface FeatureListProps {
+  children?:
+    | React.ReactElement<FeatureItemPros>[]
+    | React.ReactElement<FeatureItemPros>;
+}
+
+export const FeatureList: React.FC<FeatureListProps> = ({ children }) => (
+  <div css={(theme) => [baseStyles(theme), tabletStyles]}>{children}</div>
+);
+
+const Features = () => (
+  <FeatureList>
+    <FeatureItem
+      Icon={IconPerson}
+      title="Experienced Individuals"
+      description=" Our network is made up of highly experienced professionals who are
+          passionate about what they do."
+    />
+    <FeatureItem
+      Icon={IconCog}
+      title="Easy to Implement"
+      description="Our processes have been refined over years of implementation meaning
+          our teams always deliver."
+    />
+    <FeatureItem
+      Icon={IconChart}
+      title="Enhanced Productivity"
+      description="Our customized platform with in-built analytics helps you manage your
+          distributed teams."
+    />
+  </FeatureList>
 );
 
 export default Features;
